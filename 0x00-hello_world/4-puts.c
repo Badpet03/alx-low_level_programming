04dd9b2e90ae7b0382b94b5d229bb3fe51a1190d
Nzemeka Akponor <vakponor@gmail.com>
2022-09-08 07:44:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Short description, single line
- *   * Description: Longer description of the function)?
- *    * Return: Description of the returned value
- *     */
+ * main - Short description, single line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
