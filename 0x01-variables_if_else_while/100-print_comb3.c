e06845879f8f5b7f89336aa1a78abb6eb92778bb
Nzemeka Akponor <vakponor@gmail.com>
2022-09-11 08:00:43 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,24 +12,17 @@ int main(void)
 {
 	int d;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (d = 0; d < 100; d++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
-		digit1++;
 	}
-
 	putchar('\n');
 
 	return (0);
-}
-			
+}	
