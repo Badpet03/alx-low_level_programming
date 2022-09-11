30cac8c38ba83236c0a3131e2a9747e97a86fff9
Nzemeka Akponor <vakponor@gmail.com>
2022-09-11 11:15:00 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two two-digit numbers,ranging from 0-99
+ * main - Prints all possible combinations
  *
  * Return: Always 0.
  */
