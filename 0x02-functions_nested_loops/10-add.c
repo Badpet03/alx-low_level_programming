f3fbfafb4b5ebf564bdce969a4ac5e7e2bbf43be
Nzemeka Akponor <vakponor@gmail.com>
2022-09-13 18:22:25 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,7 @@
 
 /**
  * add - Adds two integers and outputs the result
- * @n1: integer 1 the first number to be added
- * @n1: integer 2 the second number to be added
- * Description: give two integers, returns sum
+ *
  * Return: result of addition
  */
 
