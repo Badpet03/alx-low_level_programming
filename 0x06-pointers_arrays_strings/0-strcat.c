8ce1517944b46e0aff9bf2bf602e5f6762a15f30
Nzemeka Akponor <vakponor@gmail.com>
2022-09-22 05:51:39 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int length, j; /* j will hold the length of the source */
+	int i, j;
 
-	length = 0;
-	while (dest[length] != '\0')
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while ((*(dest + i) = *(src + j)))
 	{
-		length++;
-		while (dest[length] != '\0')
-		{
-			length++;
-		}
-		for (j = 0; src[j] != '\0'; j++, length++)
-		{
-			dest[length] = src[j];
-		}
-		dest[length] = '\0';
-		return (dest);
+		i++;
+		j++;
 	}
+	return (dest);
+}
