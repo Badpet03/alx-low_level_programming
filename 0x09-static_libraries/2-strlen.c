910e62df5126d933371a8c61cc244bf70f2ce5cb
Nzemeka Akponor <vakponor@gmail.com>
2022-10-01 02:36:39 -0700

    libmy.a

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  * _strlen - Returns length of a string
  * @str: the length of string to be determined
+ *
  * Return: Length of string
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int len = 0;
 
-	while (*str++)
-		length++;
-	return (length);
+	while (*s++)
+		len++;
+	return (len);
 }
